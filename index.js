a1d4aed7c3677dd807e3c31bc81a12e111e942b5
Thomas Le Bihan <thomas.66days@gmail.com>
2021-05-03 12:26:38 +0200

    Initialize server

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const cors = require("cors");
+const data = require("./data.json");
+
+const app = express();
+app.use(cors());
+
+app.get("/", (req, res) => {
+  return res.json(data);
+});
+
+// Heroku va nous fournir une variable process.env.PORT
+// if (process.env.PORT) {
+//   app.listen(process.env.PORT, () => {
+//     console.log("Server started");
+//   });
+// } else {
+//   app.listen(3200, () => {
+//     console.log("Server started");
+//   });
+// }
+
+app.listen(process.env.PORT || 3200, () => {
+  console.log("Server started");
+});
